38f341ae0aec6df5ee9ad98aa4cb68c50a82f789
Anthony Romrell <anthonyromrell@mac.com>
2018-01-18 11:15:16 -0700

    Worked on Animal Class. Added Tiger Art from the Asset Store.

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Hey there!
+	// Use this for initialization
+	void Start () {
+		print("I love my cat.");
+		print("No, I really don't.");
+	}
+	
+	void OnMouseDown()
+	{
+		print("Meow.");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
