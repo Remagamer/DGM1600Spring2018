f670a6e7925f44c990ae61bf3f4b6ad7d92e1522
Anthony Romrell <anthonyromrell@mac.com>
2018-03-08 11:24:01 -0700

    Calculator

---
+++ FirstGame/Assets/Scripts/Functions/FunctionsWithReturns.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionsWithReturns : MonoBehaviour
+{
+	public int Health;
+
+	public int AddHealth(int value)
+	{
+		print("Hello There");
+		//move a character
+		//play an animation
+		return Health += value;
+	}
+
+	private void OnMouseDown()
+	{
+		Health = AddHealth(10);
+		Players.Add(NewPlayer("Bob"));
+	}
+
+	public List<string> Players;
+
+	public List<string> NewPlayers()
+	{
+		return new List<string>();
+	}
+
+	public string NewPlayer(string name)
+	{
+		return name;
+	}
+}
