b027e41477ea3e0308cc1dadd281b492a07a6cb4
Anthony Romrell <anthonyromrell@mac.com>
2018-04-04 16:04:57 -0600

    AI Agents

---
+++ FirstGame/Assets/AI/AIAgent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIAgent : MonoBehaviour
+{
+
+	private NavMeshAgent Agent;
+	private Animator Anims;
+
+	void Start ()
+	{
+		Agent = GetComponent<NavMeshAgent>();
+		Anims = GetComponent<Animator>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Anims.SetTrigger("ChangeState");
+	}
+}
---
+++ FirstGame/Assets/AI/AIBehaviours.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIBehaviours : StateMachineBehaviour
+{
+
+	public NavMeshAgent Agent;
+	private NavMeshAgent NewAgent;
+	public Transform Home;
+	public Transform Hunt;
+	
+    // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		Agent = GameObject.FindGameObjectWithTag("Agent").GetComponent<NavMeshAgent>();
+	}
+
+	// OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		
+		if (stateInfo.IsName("Hunt"))
+		{
+			Agent.destination = Home.position;
+		}
+		else
+		{
+			Agent.destination = Hunt.position;
+		}
+		
+	}
+
+	// OnStateExit is called before OnStateExit is called on any state inside this state machine
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called before OnStateMove is called on any state inside this state machine
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called before OnStateIK is called on any state inside this state machine
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMachineEnter is called when entering a statemachine via its Entry Node
+	override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+	{
+	}
+
+	// OnStateMachineExit is called when exiting a statemachine via its Exit Node
+	//override public void OnStateMachineExit(Animator animator, int stateMachinePathHash) {
+	//
+	//}
+}
